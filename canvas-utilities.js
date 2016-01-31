d326a8c8f7e2a203235ad843328f0f48da6a1790
Beau Gunderson <beau@beaugunderson.com>
2016-01-30 20:03:56 -0800

    add polyfill for ellipse

--- canvas-utilities.js
+++ canvas-utilities.js
@@ -1,3 +1,17 @@
+'use strict';
+
 var Canvas = require('canvas');
+
+// polyfill for the new ellipse method
+Canvas.Context2d.prototype.ellipse =
+  function (x, y, radiusX, radiusY, rotation, startAngle, endAngle,
+    antiClockwise) {
+    this.save();
+    this.translate(x, y);
+    this.rotate(rotation);
+    this.scale(radiusX, radiusY);
+    this.arc(0, 0, 1, startAngle, endAngle, antiClockwise);
+    this.restore();
+  };
 
 exports.Canvas = Canvas;
