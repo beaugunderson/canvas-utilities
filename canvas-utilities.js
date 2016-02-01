e4e074cc85a1de684b61cfc19021d55ef0ee4526
Beau Gunderson <beau@beaugunderson.com>
2016-02-01 01:00:59 -0800

    fix ellipse polyfill for the browser

--- canvas-utilities.js
+++ canvas-utilities.js
@@ -2,16 +2,24 @@
 
 var Canvas = require('canvas');
 
+function ellipse(x, y, radiusX, radiusY, rotation, startAngle, endAngle,
+  antiClockwise) {
+  this.save();
+  this.translate(x, y);
+  this.rotate(rotation);
+  this.scale(radiusX, radiusY);
+  this.arc(0, 0, 1, startAngle, endAngle, antiClockwise);
+  this.restore();
+}
+
 // polyfill for the new ellipse method
-Canvas.Context2d.prototype.ellipse =
-  function (x, y, radiusX, radiusY, rotation, startAngle, endAngle,
-    antiClockwise) {
-    this.save();
-    this.translate(x, y);
-    this.rotate(rotation);
-    this.scale(radiusX, radiusY);
-    this.arc(0, 0, 1, startAngle, endAngle, antiClockwise);
-    this.restore();
-  };
+if (Canvas.Context2d && Canvas.Context2d.prototype.ellipse === undefined) {
+  // server-side using automattic/node-canvas
+  Canvas.Context2d.prototype.ellipse = ellipse;
+} else if (typeof CanvasRenderingContext2D !== 'undefined' &&
+           CanvasRenderingContext2D.prototype.ellipse === undefined) {
+  // browser-side
+  CanvasRenderingContext2D.prototype.ellipse = ellipse;
+}
 
 exports.Canvas = Canvas;
